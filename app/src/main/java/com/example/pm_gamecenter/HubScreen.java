ad6c2d2409e92b30f885a8470d8187ce1e393f60
Xavier <chabolaxavi@gmail.com>
2024-02-29 00:56:29 +0100

    Added visual gamecards to screen_hub.xml

--- app/src/main/java/com/example/pm_gamecenter/HubScreen.java
+++ app/src/main/java/com/example/pm_gamecenter/HubScreen.java
@@ -1,6 +1,14 @@
 package com.example.pm_gamecenter;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HubScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_hub);
+    }
 }
